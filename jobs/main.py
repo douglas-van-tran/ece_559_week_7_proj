cfe4517df0d4f5fbdb6a39fdcaf0fc1d268a5284
Douglas Van Tran <214543946+douglas-van-tran@users.noreply.github.com>
2026-05-30 15:11:26 -0400

    Progress...

--- jobs/main.py
+++ jobs/main.py
@@ -3,21 +3,21 @@ import ece_559_week_7_proj
 # def main():
 
 # Define scenarios
-scenario_1 = ece_559_week_7_proj.VirtualPowerPlant(pv=0, ev_control_capacity=0.5)
-p_feed_1_no_vpp, p_sub_1_no_vpp = scenario_1.calculate_p_feed(
-    is_vpp_dispatch=False, is_winter=True
+scenario_1 = ece_559_week_7_proj.VirtualPowerPlant(p_pv=0, ev_charging_capacity=0.5)
+a, b = scenario_1.calculate_p_feed(
+    is_vpp_dispatch=False,
+    is_winter=True,
 )
-p_feed_1_vpp, p_sub_1_vpp = scenario_1.calculate_p_feed(
-    is_vpp_dispatch=True, is_winter=True
+c, d = scenario_1.calculate_p_feed(
+    is_vpp_dispatch=True,
+    is_winter=True,
 )
 
 # # Scenario 2
 # # Assume "high PV output with moderate load" is 6 MW
-scenario_2 = ece_559_week_7_proj.VirtualPowerPlant(
-    pv=5, peak_load=6, ev_control_capacity=0
-)
-x, y = scenario_2.calculate_p_feed(is_vpp_dispatch=False, bess_charge_adj=0.4)
-z, a = scenario_2.calculate_p_feed(is_vpp_dispatch=True, bess_charge_adj=0.4)
+scenario_2 = ece_559_week_7_proj.VirtualPowerPlant(p_pv=5, peak_load=6, ev_charging_capacity=0)
+e, f = scenario_2.calculate_p_feed(is_vpp_dispatch=False, bess_charge_adj=0.4)
+g, h = scenario_2.calculate_p_feed(is_vpp_dispatch=True, bess_charge_adj=0.4)
 
 # # Scenario 3
 # # Feeder fault; assume PV output is 8 MW
--- src/ece_559_week_7_proj/virtual_power_plant.py
+++ src/ece_559_week_7_proj/virtual_power_plant.py
@@ -5,59 +5,59 @@ import numpy as np
 class VirtualPowerPlant:
     def __init__(
         self,
-        pv: int,
-        ev_control_capacity: float = 1.0,
-        num_feeders: int = 2,
-        peak_load: int = 10,
-        primary_volt: float = 13.2,
+        p_load: int = 10,
+        p_ev: int = 2,
+        p_pv: int = 5,
+        p_bess: int = 5,
+        p_ev_charge_capacity: float = 1.0,
+        primary_voltage: float = 13.2,
         feeder_len: int = 5,
-        bess: int = 5,
-        ev_charge_load: int = 2,
+        num_feeders: int = 2,
     ):
-        self.pv = pv
-        self.ev_control_capacity = ev_control_capacity
-        self.num_feeders = num_feeders
-        self.peak_load = peak_load
-        self.primary_volt = primary_volt
+        self.p_load = p_load
+        self.p_ev = p_ev
+        self.p_pv = p_pv
+        self.p_bess = p_bess
+        self.p_ev_charge_capacity = p_ev_charge_capacity
+        self.primary_voltage = primary_voltage
         self.feeder_len = feeder_len
-        self.bess = bess
-        self.ev_charge_load = ev_charge_load
+        self.num_feeders = num_feeders
 
-    def _calculate_p_ev(self) -> float:
-        p_ev = self.ev_charge_load / self.num_feeders
+    # def _calculate_p_ev(self) -> float:
 
-        return p_ev
+    #     print(p_ev)
+
+    #     return p_ev
 
     def calculate_p_feed(
         self,
         is_vpp_dispatch: bool,
-        bess_charge_adj: float = 1.0,
         is_winter: bool = False,
     ) -> tuple[float, float]:
         # Calculate p_ev
-        p_ev = self._calculate_p_ev()
-
-        # Adjust p_ev and calculate bess values if vpp_dispatch is True
-        if is_vpp_dispatch:
-            # Adjust p_ev by available controllable capacity
-            p_ev *= self.ev_control_capacity
-
-            # Calculate the bess discharge rate
-            p_bess_discharge = self.bess / self.num_feeders
-
-            # If not wintertime, adjust the bess charging rate if needed
-            # Else, the bess charge rate is 0
-            if not is_winter:
-                p_bess_charge = (self.bess / self.num_feeders) * bess_charge_adj
-            else:
-                p_bess_charge = 0
+        p_ev = self.p_ev / self.num_feeders
 
         # If is_vpp_dispatch is False, bess values are 0
-        else:
-            p_bess_discharge = 0
-            p_bess_charge = 0
+        if not is_vpp_dispatch:
+            p_feed = self.p_load + p_ev
 
-        p_feed = self.peak_load + p_ev - self.pv - p_bess_discharge + p_bess_charge
+        # Adjust p_ev and calculate bess values if vpp_dispatch is True
+        else:
+            # Adjust p_ev by available controllable capacity
+            p_ev *= self.p_ev_charge_capacity
+
+            # Calculate the bess discharge rate
+            p_bess_discharge = self.p_bess / self.num_feeders
+
+            # If wintertime, the bess charge rate is 0
+            # Else, adjust the bess charging rate if needed
+            if is_winter:
+                p_bess_charge = 0
+
+            else:
+                p_bess_charge = self.p_bess / self.num_feeders
+
+            p_feed = self.p_load + p_ev - self.p_pv - p_bess_discharge + p_bess_charge
 
         p_sub = p_feed * self.num_feeders
 
