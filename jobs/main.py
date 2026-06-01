0694a32cc38b30a3c413547b38fb964f550daa6b
Douglas Van Tran <214543946+douglas-van-tran@users.noreply.github.com>
2026-05-31 21:33:11 -0400

    Progress

--- jobs/main.py
+++ jobs/main.py
@@ -2,27 +2,50 @@ import ece_559_week_7_proj
 
 # def main():
 
-# Define scenarios
-scenario_1 = ece_559_week_7_proj.VirtualPowerPlant(p_pv=0, ev_charging_capacity=0.5)
-a, b = scenario_1.calculate_p_feed(
-    is_vpp_dispatch=False,
-    is_winter=True,
-)
-c, d = scenario_1.calculate_p_feed(
-    is_vpp_dispatch=True,
-    is_winter=True,
-)
-
-# # Scenario 2
-# # Assume "high PV output with moderate load" is 6 MW
-scenario_2 = ece_559_week_7_proj.VirtualPowerPlant(p_pv=5, peak_load=6, ev_charging_capacity=0)
-e, f = scenario_2.calculate_p_feed(is_vpp_dispatch=False, bess_charge_adj=0.4)
-g, h = scenario_2.calculate_p_feed(is_vpp_dispatch=True, bess_charge_adj=0.4)
-
-# # Scenario 3
-# # Feeder fault; assume PV output is 8 MW
-# scenario_3 = ece_559_week_7_proj.VirtualPowerPlant(pv=8)
-
-
 # if __name__ == "__main__":
 #     main()
+
+# Scenario 1
+# PV = 0 MW
+# EV charging capacity is 50%
+scenario_1 = ece_559_week_7_proj.VirtualPowerPlant(
+    p_load=10,
+    p_ev=2,
+    p_pv=0,
+    p_ev_charge_capacity=0.5,
+)
+
+# Scenario 2
+# Moderate load, assume 6 MW
+# Partial BESS charging, assume 2 MW
+# No EV charging, 0 MW
+scenario_2 = ece_559_week_7_proj.VirtualPowerPlant(
+    p_load=6,
+    p_ev=0,
+    p_pv=5,
+    p_bess=2,
+)
+
+# Scenario 3
+# Assumptions:
+#   - Load = 16 MW
+#   - EV Charging = 2 MW
+#   -
+
+f1, s1 = scenario_1.calculate_p_feed(
+    is_vpp_dispatch=False,
+)
+f1_vpp, s1_vpp = scenario_1.calculate_p_feed(
+    is_vpp_dispatch=True,
+)
+print("The parameters for scenario 1 are:")
+print(f1, s1, f1_vpp, s1_vpp)
+
+f2, s2 = scenario_2.calculate_p_feed(
+    is_vpp_dispatch=False,
+)
+f2_vpp, s2_vpp = scenario_2.calculate_p_feed(
+    is_vpp_dispatch=True,
+)
+print("The parameters for scenario 1 are:")
+print(f2, s2, f2_vpp, s2_vpp)
--- src/ece_559_week_7_proj/virtual_power_plant.py
+++ src/ece_559_week_7_proj/virtual_power_plant.py
@@ -8,7 +8,7 @@ class VirtualPowerPlant:
         p_load: int = 10,
         p_ev: int = 2,
         p_pv: int = 5,
-        p_bess: int = 5,
+        p_bess: int = -5,
         p_ev_charge_capacity: float = 1.0,
         primary_voltage: float = 13.2,
         feeder_len: int = 5,
@@ -32,33 +32,21 @@ class VirtualPowerPlant:
     def calculate_p_feed(
         self,
         is_vpp_dispatch: bool,
-        is_winter: bool = False,
     ) -> tuple[float, float]:
-        # Calculate p_ev
-        p_ev = self.p_ev / self.num_feeders
 
         # If is_vpp_dispatch is False, bess values are 0
         if not is_vpp_dispatch:
-            p_feed = self.p_load + p_ev
+            p_ev = self.p_ev / self.num_feeders
+            p_bess = 0
 
         # Adjust p_ev and calculate bess values if vpp_dispatch is True
         else:
             # Adjust p_ev by available controllable capacity
-            p_ev *= self.p_ev_charge_capacity
+            p_ev = self.p_ev_charge_capacity * (self.p_ev / self.num_feeders)
+            p_bess = self.p_bess / self.num_feeders
 
-            # Calculate the bess discharge rate
-            p_bess_discharge = self.p_bess / self.num_feeders
-
-            # If wintertime, the bess charge rate is 0
-            # Else, adjust the bess charging rate if needed
-            if is_winter:
-                p_bess_charge = 0
-
-            else:
-                p_bess_charge = self.p_bess / self.num_feeders
-
-            p_feed = self.p_load + p_ev - self.p_pv - p_bess_discharge + p_bess_charge
-
+        # Calculate p_feed and p_sub
+        p_feed = self.p_load + p_ev - self.p_pv + p_bess
         p_sub = p_feed * self.num_feeders
 
         return p_feed, p_sub
